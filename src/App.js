88c81b6d8c98d048d81ed3eb0e1bc75874a89395
Julia Tan <juliajtan@gmail.com>
2022-01-09 17:34:06 +0000

    Allow user to enter lottery

--- src/App.js
+++ src/App.js
@@ -9,18 +9,42 @@ function App() {
 
   const [manager, setManager] = useState('')
   const [players, setPlayers] = useState([])
-  const [balance, setBalance] = useState('') // Note: balance is not a number - it's an object
+  const [balance, setBalance] = useState('') // Note: balance is not a number - it's an object (wrapped in a library called BignumberJS)
+  const [value, setValue] = useState('')
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
     const getManager = async () => {
+      // Don't need to set from argument because we're using the Metamask provider
       const manager = await lottery.methods.manager().call()
       const players = await lottery.methods.getPlayers().call()
       const balance = await web3.eth.getBalance(lottery.options.address)
       setManager(manager)
+      setPlayers(players)
+      setBalance(balance)
     }
 
     getManager()
-  }, [])
+  }, [manager, players, balance])
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+
+    // Need to define from argument when using send method
+    const accounts = await web3.eth.getAccounts()
+
+    setMessage('Waiting on transaction success...')
+
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(value, 'ether'),
+      })
+      setMessage('You have been entered!')
+    } catch (error) {
+      setMessage('Sorry, something went wrong')
+    }
+  }
 
   return (
     <div>
@@ -29,6 +53,18 @@ function App() {
         This contract is managed by {manager}. There are currently {players.length} people entered, competing to win{' '}
         {web3.utils.fromWei(balance, 'ether')} ether!
       </p>
+      <hr />
+
+      <form onSubmit={onSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input value={value} onChange={(e) => setValue(e.target.value)} />
+        </div>
+        <button>Enter</button>
+      </form>
+      <hr />
+      <h1>{message}</h1>
     </div>
   )
 }
