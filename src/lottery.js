682ad0dbd842eaa90019b4b396a9462472703003
Julia Tan <juliajtan@gmail.com>
2022-01-09 15:56:45 +0000

    Add ABI and deployment contract address to create local copy of lottery smart contract

---
+++ src/lottery.js
@@ -0,0 +1,54 @@
+import web3 from './web3'
+
+const address = '0xAE7938AC7AB7D87e8B82239CB9FA2FE37788fE20'
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'manager',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'pickWinner',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'getPlayers',
+    outputs: [{ name: '', type: 'address[]' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'enter',
+    outputs: [],
+    payable: true,
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{ name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  { inputs: [], payable: false, stateMutability: 'nonpayable', type: 'constructor' },
+]
+
+// This is the local copy of our smart contract - not the actual contract on the blockchain
+export default new web3.eth.Contract(abi, address)
